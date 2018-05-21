0d49c5dceb77e955141359f23c92917b81b55c8e
yakovina <sasha.yakovina@gmail.com>
2018-05-21 19:25:24 +0300

    html-changes

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass');
+
+
+
+gulp.task('sass', function(){
+    return gulp.src(['src/sass/**/*.sass', 'src/sass/**/*.scss'])
+    .pipe(sass({outputStyle: "expanded"}).on('error', sass.logError))
+    .pipe(gulp.dest('css'))
+    });
+
+gulp.task('watch',function(){
+    gulp.watch(['src/sass/**/*.sass', 'src/sass/**/*.scss'],['sass']);
+});
+
+
+
+gulp.task('default', ['watch']);
